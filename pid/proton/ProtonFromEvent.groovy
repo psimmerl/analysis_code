972bb5204a8cb9288fffb88285cbd64ef1c257a3
Sangbaek <sangbaek@mit.edu>
2020-05-24 19:25:42 -0400

    proton dcr not applied to CD

--- pid/proton/ProtonFromEvent.groovy
+++ pid/proton/ProtonFromEvent.groovy
@@ -237,7 +237,7 @@ class ProtonFromEvent {
 
   def passProtonDCR1 = { event, index ->
 
-  if (event.status[index]<4000) return true
+  if (event.status[index]>4000) return true
 
   if (event.dc1_status.contains(index)){
       def sec = event.dc_sector.get(index)-1
@@ -255,7 +255,7 @@ class ProtonFromEvent {
 
   def passProtonDCR2 = { event, index ->
 
-  if (event.status[index]<4000) return true
+  if (event.status[index]>4000) return true
 
   if (event.dc1_status.contains(index)){
       def sec = event.dc_sector.get(index)-1
@@ -273,7 +273,7 @@ class ProtonFromEvent {
 
   def passProtonDCR3 = { event, index ->
 
-  if (event.status[index]<4000) return true
+  if (event.status[index]>4000) return true
 
   if (event.dc1_status.contains(index)){
       def sec = event.dc_sector.get(index)-1
