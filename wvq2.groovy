053ad1dddbe7ff846de6614421e552bf252c8eee
Paul Simmerling <psimmerl@ifarm1802.jlab.org>
2020-07-13 11:57:24 -0400

    fixed histograms

--- wvq2.groovy
+++ wvq2.groovy
@@ -3,12 +3,16 @@ import org.jlab.detector.base.DetectorType
 import org.jlab.clas.physics.Particle
 import org.jlab.clas.physics.LorentzVector
 import org.jlab.clas.physics.Vector3
-import org.jlab.groot.data.H1F
+import org.jlab.groot.data.H2F
 import org.jlab.groot.data.TDirectory
 
 //import pid.electron.Electron
 
-def hW = new H1F("W","W",100,1,4)
+def hW = [new H2F("h0","WvQ2,(0,1]",10,0,1,10,0,10),new H2F("h1","WvQ2,(1,2]",10,1,2,10,0,10),
+          new H2F("h2","WvQ2,(2,3]",10,2,3,10,0,10),new H2F("h3","WvQ2,(3,4]",10,3,4,10,0,10),
+          new H2F("h4","WvQ2,(4,5]",10,4,5,10,0,10),new H2F("h5","WvQ2,(5,6]",10,5,6,10,0,10),
+          new H2F("h6","WvQ2,(6,7]",10,6,7,10,0,10),new H2F("h7","WvQ2,(7,8]",10,7,8,10,0,10),
+          new H2F("h8","WvQ2,(8,9]",10,8,9,10,0,10),new H2F("h9","WvQ2,(9,10]",10,9,10,10,0,10)]
 
 def beam    = LorentzVector.withPID(11,0,0,10.6)
 def target  = LorentzVector.withPID(2212,0,0,0)
@@ -26,9 +30,13 @@ for(fname in args) {
          def my_ele = (0..<partb.rows()
             ).findAll{partb.getInt('pid',it)==11 && partb.getShort('status',it)<0
             }.findResults{iele->
-               //def ele = 
-               return LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
-               //return ele.e()>1.5 ? ele : null
+               def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
+               def eX  = beam+target-ele
+               def Q2  = (beam.p()-eX.p())**2
+               
+               hW[floor(Q2)].fill(Q2,eX.mass())
+               
+               return ele.e()>1.5 ? ele : null
             }
          //print my_ele
          //hW.fill(my_ele.mass())
