d41646c135de6986f22f0ab7d213312d06557b5e
Paul Simmerling <paul.simmerling@uconn.edu>
2020-07-14 14:58:34 -0400

    added fill of hists

--- wvq2.groovy
+++ wvq2.groovy
@@ -7,6 +7,11 @@ import org.jlab.groot.data.H2F
 import org.jlab.groot.data.TDirectory
 import javax.swing.JFrame
 import org.jlab.groot.graphics.EmbeddedCanvas
+import utils.KinTool
+import my.Sugar
+
+Sugar.enable()
+
 
 //import pid.electron.Electron
 println "starting"
@@ -17,7 +22,7 @@ println "made canvas"
 
 def hW = []
 for(int i = 0; i < 1 /*6*/; i++ ) {
-   for(int j = 0; j < 10; j++) {
+   for(int j = 0; j < 10; j+=1) {
       hW.add(new H2F("h-${i}-${j}",
          "WvQ^{2} Sector ${i};Q^{2} (GeV);W (GeV/c^{2}?)",10,j,j+1,10,0,10))
    }
@@ -27,9 +32,10 @@ println "made hists"
 def beam    = LorentzVector.withPID(11,0,0,10.6)
 def target  = LorentzVector.withPID(2212,0,0,0)
 
-canvas.Divide(5, 2)
+canvas.divide(5, 2)
 println "divided"
 for(fname in args) {
+   def skipped = 0
    def reader = new HipoDataSource()
    reader.open(fname)
    println "open"
@@ -40,15 +46,22 @@ for(fname in args) {
          //def calb  = event.getBank("REC::Calorimeter")
          def my_ele = (0..<partb.rows()
             ).findAll{partb.getInt('pid',it)==11 && partb.getShort('status',it)<0//TODO: Vaguely remember this means only trigger electron, if so I should remove
-            }.findResults{iele->
+            }.each{iele->//findResults{iele->
                def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
                def eX  = beam+target-ele
-               def Q2  = (beam.p()-eX.p())**2
-               
-               hW[floor(Q2)].fill(Q2,eX.mass())
+               //def Q2  = (beam.p()-eX.p())**2
+               def Q2 = KinTool.calcQ2(beam, ele)
+               //println Q2
+               if (Q2<10){ 
+                  hW[Math.floor(Q2)].fill(Q2,eX.mass())//Not sure if this is what W & Q2 are
+               } else {
+                  println Q2
+                  skipped++
+               }
+               //hW[Math.floor(Q2)].fill(Q2,eX.mass())//Not sure if this is what W & Q2 are
                //TODO: Need to figure out how to split based on azimuthal sector
                
-               return ele.e()>1.5 ? ele : null
+              // return ele.e()>1.5 ? ele : null
             }
          //print my_ele
          //hW.fill(my_ele.mass())
@@ -56,7 +69,7 @@ for(fname in args) {
       }  
    }
    reader.close()
-   println "closed"
+   println "Skipped ${skipped} events from ${fname}"
 }
 
 
@@ -65,14 +78,13 @@ hW.eachWithIndex{it,index->
    canvas.cd(index)
    //it.getXaxis().SetTitle("Q^{2} (GeV)"
    canvas.draw(it)
+   out.addDataset(it)
    println "draw"
 }
 frame.add(canvas)
 frame.setLocationRelativeTo(null)
 frame.setVisible(true)
-out.add(canvas)
-out.addDataset(hW)
+//out.add(canvas)
+//out.addDataset(hW)
 out.writeFile('wvq2.hipo')
-
-
 
