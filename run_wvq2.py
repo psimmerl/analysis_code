24ee64e5f6d811d7691a3f4d6cacd92148ad2d1f
Paul Simmerling <paul.simmerling@uconn.edu>
2020-07-16 14:59:55 -0400

    secs

--- run_wvq2.py
+++ run_wvq2.py
@@ -1,14 +1,14 @@
 import ROOT, sys, os
 
-os.system("run-groovy wvq2.groovy skim4_005032.hipo")
+#os.system("run-groovy wvq2.groovy skim4_005032.hipo")
 
 ff =  ROOT.TFile('q2w_hs.root')#sys.argv[1])
-h1s = [ff.Get("hq2w_sec"+str(sec)+"_q2bin"+str(q2bin)) for sec in range(1,2) for q2bin in range(10)]
-
+h1s = [ff.Get("hq2w_sec"+str(sec)+"_q2bin"+str(q2bin)) for sec in range(1,7) for q2bin in range(10)]
 c1 = ROOT.TCanvas("c1","c1",1100,800)
 c1.Divide(5,2)
-for sec in range(1):
+for sec in range(6):
    for i in range(10):
       c1.cd(i+1)
+      #h1s[sec*10+i].SetHistLineColor(sec)
       h1s[sec*10+i].Draw("same")
 c1.Print("out.pdf")
